mistake in bf and normalization fixed to get mixed species inelastic components

boundfree_dsf.py
@@ -4,6 +4,7 @@ from plasma_state import PlasmaState
 from models import ModelOptions
 from unit_conversions import *
 from constants import *
+from utils import read_mcss_output
 from scipy.special import gamma
 import numpy as np
 
@@ -69,6 +70,7 @@ class BoundFreeDSF:
         """
 
         Sce = 0.0
+        J = np.zeros_like(w)
 
         if Zb > 0:
             c1s = 0
@@ -103,9 +105,8 @@ class BoundFreeDSF:
             # dimensionless scattering wave number
             q = (w_freq - wC) / (SPEED_OF_LIGHT * k)
 
-            J = 0.0
-
             if c1s > 0:
+
                 n = 1
                 l = 0
 
@@ -113,11 +114,12 @@ class BoundFreeDSF:
                 xnl = 1.0 / (1.0 + (n * q / (Znl * FINE_STRUCTURE_CONSTANT)) ** 2.0)  # [xnl] =
 
                 Jnl = self._shell_amplitude(Znl, n, l) * xnl**3 / 3
-
+                # Jnl10 = Jnl
                 for i in range(c1s):
-                    if Eb[i] >= 0:
+                    if Eb[i] > 0:
                         continue
-                    J = J + Jnl * np.heaviside(E + Eb[i], 1)  # np.heaviside(E, Eb[i])
+                    J += Jnl * np.heaviside(E + Eb[i], 1)  # np.heaviside(E, Eb[i])
+                    # Jnl10 *= np.heaviside(E + Eb[i], 1)
 
             if c2s > 0:
 
@@ -127,18 +129,13 @@ class BoundFreeDSF:
                 Znl = self.ff_model.calculate_effective_charge_state(ZA, Zb, n, l)
                 xnl = 1.0 / (1.0 + (n * q / (Znl * FINE_STRUCTURE_CONSTANT)) ** 2.0)
 
-                Jnl = (
-                    self._shell_amplitude(Znl, n, l)
-                    * 4.0
-                    * (xnl**3.0 / 3.0 - xnl**4.0 + 4.0 * xnl**5.0 / 5.0)
-                    * 1.0e-6
-                )
-
+                Jnl = self._shell_amplitude(Znl, n, l) * 4.0 * (xnl**3.0 / 3.0 - xnl**4.0 + 4.0 * xnl**5.0 / 5.0)
+                # Jnl20 = Jnl
                 for i in range(c2s):
-                    if Eb[i + 2] >= 0:
+                    if Eb[i + 2] > 0:
                         continue
-                    # J = J + Jnl * np.heaviside(E, Eb[i + 2 - 1])
-                    J = J + Jnl * np.heaviside(E + Eb[i + 2], 1)
+                    J += Jnl * np.heaviside(E + Eb[i + 2], 1)
+                    # Jnl20 *= np.heaviside(E + Eb[i + 2], 1)
 
             if c2p > 0:
 
@@ -149,12 +146,12 @@ class BoundFreeDSF:
                 xnl = 1.0 / (1.0 + (n * q / (Znl * FINE_STRUCTURE_CONSTANT)) ** 2.0)
 
                 Jnl = self._shell_amplitude(Znl, n, l) * (xnl**4.0 / 4.0 - xnl**5.0 / 5.0)
-
+                # Jnl21 = Jnl
                 for i in range(c2p):
-                    if Eb[i + 4] >= 0:
+                    if Eb[i + 4] > 0:
                         continue
-                    # J = J + Jnl * np.heaviside(E, Eb[i + 4 - 1])
-                    J = J + Jnl * np.heaviside(E + Eb[i + 4], 1)
+                    J += Jnl * np.heaviside(E + Eb[i + 4], 1)
+                    # Jnl21 *= np.heaviside(E + Eb[i + 4], 1)
 
             if c3s > 0:
 
@@ -174,8 +171,7 @@ class BoundFreeDSF:
                 for i in range(c3s):
                     if Eb[i + 10] >= 0:
                         continue
-                    # J = J + Jnl * np.heaviside(E, Eb[i + 10 - 1])
-                    J = J + Jnl * np.heaviside(E + Eb[i + 10], 1)
+                    J += Jnl * np.heaviside(E + Eb[i + 10], 1)
 
             if c3p > 0:
 
@@ -193,8 +189,7 @@ class BoundFreeDSF:
                 for i in range(c3p):
                     if Eb[i + 12] >= 0:
                         continue
-                    # J = J + Jnl * np.heaviside(E, Eb[i + 12 - 1])
-                    J = J + Jnl * np.heaviside(E + Eb[i + 12], 1)
+                    J += Jnl * np.heaviside(E + Eb[i + 12], 1)
 
             if c4s > 0:
                 n = 4
@@ -219,8 +214,7 @@ class BoundFreeDSF:
                 for i in range(c4s):
                     if Eb[i + 18] >= 0:
                         continue
-                    # J = J + Jnl * np.heaviside(E, Eb[i + 18 - 1])
-                    J = J + Jnl * np.heaviside(E + Eb[i + 18], 1)
+                    J += Jnl * np.heaviside(E + Eb[i + 18], 1)
 
             if c3d > 0:
 
@@ -234,9 +228,9 @@ class BoundFreeDSF:
                 for i in range(c3d):
                     if Eb[i + 20] >= 0:
                         continue
-                    # J = J + Jnl * np.heaviside(E, Eb[i + 20 - 1])
-                    J = J + Jnl * np.heaviside(E + Eb[i + 20], 1)
+                    J += Jnl * np.heaviside(E + Eb[i + 20], 1)
 
+            # Sce = (c1s * Jnl10 + c2s * Jnl20 + c2p * Jnl21) / (SPEED_OF_LIGHT * k)
             Sce = J / (SPEED_OF_LIGHT * k)
 
             # Detailed balanced

plasma_state.py
@@ -24,10 +24,30 @@ def get_frac(Z, Z_min, Z_max):
     return frac_min, frac_max
 
 
+def get_frac_partial(Z, Z_min, Z_max, xlim):
+    frac_min = 0
+    frac_max = 0
+    tot = 1.0 - xlim
+
+    if Z_min != Z_max:
+        frac_max = tot * (Z - Z_min) / (Z_max - Z_min)
+        frac_min = tot - frac_max
+    else:
+        frac_max = tot
+        frac_min = 0.0
+    return frac_min, frac_max
+
+
 def get_fractions_from_Z(Z):
     Z_min, Z_max = get_Z(Z)
     frac_min, frac_max = get_frac(Z=Z, Z_min=Z_min, Z_max=Z_max)
-    return frac_min, frac_max
+    return Z_min, Z_max, frac_min, frac_max
+
+
+def get_fractions_from_Z_partial(Z, x0=0):
+    Z_min, Z_max = get_Z(Z)
+    frac_min, frac_max = get_frac_partial(Z=Z, Z_min=Z_min, Z_max=Z_max, xlim=x0)
+    return Z_min, Z_max, frac_min, frac_max
 
 
 def get_rho_T_from_rs_theta(rs, theta, atomic_mass=1.00784):
@@ -51,6 +71,16 @@ def get_rho_T_from_rs_theta(rs, theta, atomic_mass=1.00784):
     return rho, T
 
 
+def get_rho_T_from_rs_theta_SI(rs, theta, atomic_mass):
+    fermi_energy = DIRAC_CONSTANT**2 / (2 * ELECTRON_MASS) * (9 * np.pi / (4 * (rs * BOHR_RADIUS) ** 3)) ** (2 / 3)
+    Te = fermi_energy * theta / BOLTZMANN_CONSTANT
+    T = Te  # * K_TO_eV
+
+    m_atm = atomic_mass  #  * 1.6605e-27
+    rho = 3 / (4 * np.pi * (rs * BOHR_RADIUS) ** 3) * m_atm
+    return rho, T
+
+
 def get_rs_theta_from_rho_T(rho, T, atomic_mass=1.00784):
     m_atm = atomic_mass * 1.6605e-27
     rs = (m_atm / 1000 * 3 / (4 * np.pi * rho)) ** (1 / 3) * 1 / BOHR_RADIUS
@@ -61,7 +91,7 @@ def get_rs_theta_from_rho_T(rho, T, atomic_mass=1.00784):
     return rs, theta
 
 
-def get_rs_theta_from_rho_T_SI(rho, T, atomic_mass=1.00784):
+def get_rs_theta_from_rho_T_SI(rho, T, atomic_mass):
     m_atm = atomic_mass  # * 1.6605e-27
     rs = (m_atm * 3 / (4 * np.pi * rho)) ** (1 / 3) * 1 / BOHR_RADIUS
 

xdave.py
@@ -1,4 +1,4 @@
-from plasma_state import PlasmaState, get_rho_T_from_rs_theta, get_fractions_from_Z
+from plasma_state import PlasmaState, get_rho_T_from_rs_theta, get_fractions_from_Z, get_fractions_from_Z_partial
 from models import ModelOptions
 from freefree_dsf import FreeFreeDSF
 from boundfree_dsf import BoundFreeDSF
@@ -11,6 +11,9 @@ from utils import (
     get_atomic_mass_for_element,
     get_binding_energies_from_elements,
     get_emission_lines_for_element,
+    calculate_angle,
+    get_mcss_wr_from_status_file,
+    load_mcss_result,
 )
 from unit_conversions import *
 from constants import *
@@ -63,7 +66,7 @@ class Setup:
             amu, AN = get_atomic_mass_for_element(element)
             x = self.partial_densities[i]
             Z = self.charge_states[i]
-            assert Z <= AN, f"Ionization degree for state {i} is larger than the atomic number{AN}. Check your setup."
+            assert Z <= AN, f"Ionization degree for state {i} is larger than the atomic number {AN}. Check your setup."
             binding_energies = get_binding_energies_from_elements(AN)
             state = PlasmaState(
                 electron_temperature=self.electron_temperature,
@@ -80,9 +83,9 @@ class Setup:
             amu_mean += x * amu
 
         overlord_state = PlasmaState(
-            electron_temperature=state.electron_temperature,
-            ion_temperature=state.ion_temperature,
-            mass_density=state.mass_density,
+            electron_temperature=self.electron_temperature,
+            ion_temperature=self.ion_temperature,
+            mass_density=self.mass_density,
             charge_state=Z_mean,
             atomic_mass=amu_mean,
             atomic_number=AN_mean,
@@ -99,7 +102,6 @@ class xDave:
         overlord_state: PlasmaState,
         states: np.array,
         fractions: np.array,
-        # binding_energies: np.array,
         rayleigh_weight: float,
         sif: np.array,
         ipd: float = None,
@@ -107,7 +109,6 @@ class xDave:
         self.models = models
         self.states = states
         self.fractions = fractions
-        # self.binding_energies = binding_energies
         self.rayleigh_weight = rayleigh_weight
         self.sif = sif
         self.ipd_eV = ipd
@@ -118,10 +119,7 @@ class xDave:
 
         self._print_logo()
 
-        ff_tot = np.zeros_like(w)
-        bf_tot = np.zeros_like(w)
-
-        lfc_kernel = LFC(models=self.models, state=self.overlord_state)
+        lfc_kernel = LFC(state=self.overlord_state)
         lfc = lfc_kernel.calculate_lfc(k=k, w=w, model=self.models.lfc_model)
         print(f"Calculated LFC={lfc}")
 
@@ -132,8 +130,16 @@ class xDave:
             ipd = get_ipd(state=self.overlord_state, model=self.models.ipd_model, user_defined_ipd=self.ipd_eV)
             print(f"Calculated IPD={ipd * J_TO_eV} eV")
 
+        ff = FreeFreeDSF(state=self.overlord_state)
+        ff_dsf = ff.get_dsf(k=k, w=w, lfc=lfc, model=self.models.polarisation_model)
+        # the factor of Z/AN is needed to match MCSS results, I will need to figure out where it comes from
+        ff_tot = ff_dsf * self.overlord_state.charge_state / self.overlord_state.atomic_number
+
+        bf_tot = np.zeros_like(w)
         ff_i = np.zeros((len(self.states), len(w)))
         bf_i = np.zeros((len(self.states), len(w)))
+
+        print(f"Mean charge state = {self.overlord_state.charge_state}.")
 
         for i in range(0, len(self.states)):
             state = self.states[i]
@@ -141,9 +147,6 @@ class xDave:
             print(f"\nRunning state {i} with Z={state.charge_state} and x={x}\n")
             binding_energies = state.binding_energies * eV_TO_J
 
-            ff = FreeFreeDSF(state=state)
-            ff_dsf = ff.get_dsf(k=k, w=w, lfc=lfc, model=self.models.polarisation_model)
-            ff_tot += x * ff_dsf
             ff_i[i] = x * ff_dsf
 
             Eb = binding_energies - ipd
@@ -155,12 +158,14 @@ class xDave:
 
             bf = BoundFreeDSF(state=state)
             bf_dsf = bf.get_dsf(ZA=state.atomic_number, Zb=state.Zb, k=k, w=w, Eb=Eb, model=self.models.bf_model)
-            bf_tot += x * bf_dsf
-            bf_i[i] = x * bf_dsf
+            bf_tot += x * bf_dsf  # * state.charge_state  # / state.atomic_number
+            bf_i[i] = x * bf_dsf  # * state.charge_state  # / state.atomic_number
 
             # This is where the HNC stuff will have to go eventually
             WR = self.rayleigh_weight
 
+        # Divide by the atomic number to be consistent with the ff component
+        bf_tot /= self.overlord_state.atomic_number
         dsf = ff_tot + bf_tot
         return bf_tot, ff_tot, dsf, WR, ff_i, bf_i
 
@@ -175,17 +180,19 @@ class xDave:
         return laplace(tau=tau, E=w, wff=ff, wbf=bf)
 
     def _print_logo(self):
-        # ' .----------------.  .----------------.  .----------------.  .----------------. '
-        # '| .--------------. || .--------------. || .--------------. || .--------------. |'
-        # '| |  ____  ____  | || |  _______     | || |  _________   | || |    _______   | |  '
-        # '| | |_  _||_  _| | || | |_   __ \    | || | |  _   _  |  | || |   /  ___  |  | |  '
-        # '| |   \ \  / /   | || |   | |__) |   | || | |_/ | | \_|  | || |  |  (__ \_|  | |  '
-        # '| |    > `' <    | || |   |  __ /    | || |     | |      | || |   '.___`-.   | |  '
-        # '| |  _/ /'`\ \_  | || |  _| |  \ \_  | || |    _| |_     | || |  |`\____) |  | |  '
-        # '| | |____||____| | || | |____| |___| | || |   |_____|    | || |  |_______.'  | |  '
-        # '| |              | || |              | || |              | || |              | |  '
-        # '| '--------------' || '--------------' || '--------------' || '--------------' |  '
-        # '' '----------------'  '----------------'  '----------------'  '----------------'  '
+        # print(
+        #     ".----------------.  .----------------.  .----------------.  .----------------.\n"
+        #     "| .--------------. || .--------------. || .--------------. || .--------------. |\n"
+        #     "| |  ____  ____  | || |  _______     | || |  _________   | || |    _______   | |  \n"
+        #     "| | |_  _||_  _| | || | |_   __ \    | || | |  _   _  |  | || |   /  ___  |  | |  \n"
+        #     "| |   \ \  / /   | || |   | |__) |   | || | |_/ | | \_|  | || |  |  (__ \_|  | |  \n"
+        #     "| |    > `' <    | || |   |  __ /    | || |     | |      | || |   '.___`-.   | |  \n"
+        #     "| |  _/ /'`\ \_  | || |  _| |  \ \_  | || |    _| |_     | || |  |`\____) |  | |  \n"
+        #     "| | |____||____| | || | |____| |___| | || |   |_____|    | || |  |_______.'  | |  \n"
+        #     "| |              | || |              | || |              | || |              | |  \n"
+        #     "| '--------------' || '--------------' || '--------------' || '--------------' |  \n"
+        #     "' '----------------'  '----------------'  '----------------'  '----------------'  \n"
+        # )
         print("\n -------------------------------- \n xDAVE C\n --------------------------------\n")
 
 
@@ -194,16 +201,14 @@ def test_setup():
     rho = 1 * g_per_cm3_TO_kg_per_m3
     T = 70 * eV_TO_K
 
-    partial_densities = np.array([0.0, 0.3, 0.34, 0.36])
+    partial_densities = np.array([0.15, 0.15, 0.34, 0.36])
     charge_states = np.array([0.0, 1.0, 3, 4])
     user_defined_inputs = None
 
-    models = ModelOptions(
-        polarisation_model="NUMERICAL", bf_model="SCHUMACHER", lfc_model="DORNHEIM_ESA", ipd_model="STEWART_PYATT"
-    )
+    models = ModelOptions(polarisation_model="NUMERICAL", bf_model="SCHUMACHER", lfc_model="NONE", ipd_model="NONE")
     # binding_energies = np.array([-13.6, -130]) * eV_TO_J
 
-    c_emission_lines = get_emission_lines_for_element(element="C")
+    # c_emission_lines = get_emission_lines_for_element(element="C")
 
     setup = Setup(
         mass_density=rho,
@@ -216,10 +221,19 @@ def test_setup():
         user_defined_inputs=user_defined_inputs,
     )
     states = setup.states
-    # print(states)
-    rayleigh_weight = 0.3
     omega_array = np.linspace(-1000, 1500, 1000) * eV_TO_J
 
+    k = 8 / ang_TO_m
+    q = k * BOHR_RADIUS
+    beam_energy = 9.0e3
+    angle = calculate_angle(q=q, energy=beam_energy)
+    print(f"Running at q={q}, E={beam_energy} -> angle={angle}")
+
+    # Load values from MCSS output files
+    mcss_fn = f"mcss_tests/mixed_species_tests/mcss_mixed_species_test_ch_angle={angle:.2f}"
+    rayleigh_weight = get_mcss_wr_from_status_file(mcss_fn + "_status.txt")
+    mcss_En, mcss_wff, mcss_wbf, mcss_ff, mcss_bf, mcss_el = load_mcss_result(mcss_fn + ".csv")
+    mcss_ipd = -3.3087805e001  # eV
     sif = np.zeros_like(omega_array)
 
     kernel = xDave(
@@ -228,44 +242,44 @@ def test_setup():
         fractions=partial_densities,
         rayleigh_weight=rayleigh_weight,
         overlord_state=setup.overlord_state,
-        ipd=None,
+        ipd=0.0,
         sif=sif,
     )
 
-    k = 8 / ang_TO_m
-    q = k * BOHR_RADIUS
-
     bf_tot, ff_tot, dsf, WR, ff_i, bf_i = kernel.run(k=k, w=omega_array)
-    fig, axes = plt.subplots(1, 4, figsize=(14, 10))
+    fig, axes = plt.subplots(2, 2, figsize=(14, 10))
 
-    ax = axes[0]
+    mcss_norm = 0.3 * 1 + (1 - 0.3) * 6
+
+    ax = axes[0, 0]
     ax.set_title("Total DSF")
-    for key, value in c_emission_lines.items():
-        print(key, value)
-        # if value > 0.0:
-        ax.axvline(x=-value, label=f"{key}", ls=":")
-        # print("{} is of the type {} and is located at {}".format(key, value["type"], value["locatie"]))
     ax.plot(omega_array * J_TO_eV, bf_tot / J_TO_eV, label="BF")
     ax.plot(omega_array * J_TO_eV, ff_tot / J_TO_eV, label="FF")
     ax.plot(omega_array * J_TO_eV, dsf / J_TO_eV, label="Tot")
+    ax.plot(mcss_En, (mcss_wbf + mcss_wff) / mcss_norm, lw=2, c="black", label="MCSS / AN")
+    ax.plot(mcss_En, (mcss_wbf + mcss_wff), lw=2, c="black", ls="dashed", label="MCSS")
     ax.legend()
 
-    ax = axes[1]
+    ax = axes[1, 0]
     ax.set_title("FF contributions")
     ax.plot(omega_array * J_TO_eV, ff_i[0] / J_TO_eV, label="H0: FF")
     ax.plot(omega_array * J_TO_eV, ff_i[1] / J_TO_eV, label="H1: FF")
     ax.plot(omega_array * J_TO_eV, ff_i[2] / J_TO_eV, label="C3: FF")
     ax.plot(omega_array * J_TO_eV, ff_i[3] / J_TO_eV, label="C4: FF")
     ax.plot(omega_array * J_TO_eV, ff_tot / J_TO_eV, label="Tot FF")
+    ax.plot(mcss_En, mcss_wff / mcss_norm, lw=2, c="black", ls="dashed", label="MCSS / AN")
+    ax.plot(mcss_En, mcss_wff, lw=2, c="black", ls="solid", label="MCSS")
     ax.legend()
 
-    ax = axes[2]
+    ax = axes[1, 1]
     ax.set_title("BF contributions")
     ax.plot(omega_array * J_TO_eV, bf_i[0] / J_TO_eV, label="H0: BF")
     ax.plot(omega_array * J_TO_eV, bf_i[1] / J_TO_eV, label="H1: BF")
     ax.plot(omega_array * J_TO_eV, bf_i[2] / J_TO_eV, label="C3: BF")
     ax.plot(omega_array * J_TO_eV, bf_i[3] / J_TO_eV, label="C4: BF")
     ax.plot(omega_array * J_TO_eV, bf_tot / J_TO_eV, label="Tot BF")
+    ax.plot(mcss_En, mcss_wbf / mcss_norm, lw=2, c="black", ls="dashed", label="MCSS")
+    ax.plot(mcss_En, mcss_wbf, lw=2, c="black", ls="solid", label="MCSS")
     ax.legend()
 
     sif = stats.norm.pdf(omega_array, 0, 2 * eV_TO_J)
@@ -274,7 +288,7 @@ def test_setup():
 
     inelastic, elastic, spectrum = kernel.convolve_with_sif(sif=sif, bf=bf_tot, ff=ff_tot, WR=WR)
 
-    ax = axes[3]
+    ax = axes[0, 1]
     ax.set_title("Spectrum")
     ax.plot(omega_array * J_TO_eV, inelastic / np.max(spectrum), label="inel", ls="-.")
     ax.plot(omega_array * J_TO_eV, elastic / np.max(spectrum), label="el", ls="-.")
@@ -317,14 +331,6 @@ def test_be():
         atomic_mass=atomic_mass,
         atomic_number=4,
     )
-    ##TODO(Hannah):
-    ## check that the mass density and number of electrons is being handled correctly across all states
-    ## compare against MCSS and PIMC for this set of conditions
-    ## Add IPD model
-    ## Clean up bf call (arguments are a bit messy)
-    ## Start calculating things like kF, EF, omega_p, etc. for the plasma state upon initialisation to avoid extra computation
-    ## Start timing and looking at how much this scales with number of points
-    ## I should move away from defining states by their mass density (problematic when you have mixed species) and just look at electron number density... probably a lot easier to split up
 
     beam_energy = 9.0e3  # * eV_TO_J
     angles = np.array([13, 30, 45, 60, 80, 100, 120, 140, 160])
@@ -396,6 +402,14 @@ def test_be():
     fig.savefig(f"beryllium_test_rs={rs}_theta={theta}_Z={Z_mean}_q={q:.2f}.pdf", dpi=200)
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    test_setup()
+#     ##TODO(Hannah):
+#     ## check that the mass density and number of electrons is being handled correctly across all states
+#     ## compare against MCSS and PIMC for this set of conditions
+#     ## Add IPD model: DONE
+#     ## Clean up bf call (arguments are a bit messy): DONE
+#     ## Start calculating things like kF, EF, omega_p, etc. for the plasma state upon initialisation to avoid extra computation
+#     ## Start timing and looking at how much this scales with number of points
+#     ## I should move away from defining states by their mass density (problematic when you have mixed species) and just look at electron number density... probably a lot easier to split up: STILL THINKING ABOUT THIS
+#     test_setup()