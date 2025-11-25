f72ac77b92f0bb62900967764fb8bba49add2cfa
Hannah Bellenbaum <h.bellenbaum@hzdr.de>
2025-11-25 20:59:26 +0100

    unnecessary ndtt comparisons removed from screening cloud and normalization check

--- tests/test_screening_cloud.py
+++ tests/test_screening_cloud.py
@@ -1,12 +1,6 @@
-import sys
-
-sys.path.insert(1, "./mcss_tests")
-
 from xdave import *
 from xdave.screening_cloud import ScreeningCloud
 from xdave.constants import BOHR_RADIUS
-
-from run_mcss_sim import run_ch_ar_mode
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -40,8 +34,6 @@ def test_Fig1b():
     elements = np.array(["H", "C"])
     partial_densities = np.array([0.71, 0.29])
     charge_states = np.array([ZH, ZC])
-
-    mcss_result = run_ch_ar_mode(T=T, rho=rho, ZH=ZH, ZC=ZC, xH=partial_densities[0], angle=10)
 
     kernel = xDave(
         mass_density=rho,
@@ -79,8 +71,6 @@ def test_Fig1b():
     ax.plot(k * BOHR_RADIUS, f_dh_C, label="C: DH", ls="-.", c="orange")
     ax.plot(k * BOHR_RADIUS, f_dh_H, label="H: DH", ls="-.", c="limegreen")
     ax.plot(k * BOHR_RADIUS, f_fws_H, label="H: FWS", ls="solid", c="limegreen")
-    ax.plot(mcss_result[0], mcss_result[5], label="MCSS C: FWS", ls="solid", c="crimson")
-    ax.plot(mcss_result[0], mcss_result[4], label="MCSS H: FWS", ls="solid", c="darkgreen")
     ax.scatter(dat_C[:, 0], dat_C[:, 1], label="Chapman 2015: C", marker="x", c="crimson")
     ax.scatter(dat_H[:, 0], dat_H[:, 1], label="Chapman 2015: H", marker="x", c="darkgreen")
     ax.legend()
