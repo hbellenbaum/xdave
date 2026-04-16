a674abd5f45ba7036d253e5a77e2f63b439cc695
Hannah Bellenbaum <h.bellenbaum@hzdr.de>
2026-04-16 23:58:17 +0200

    conf file modified

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,6 @@ from pathlib import Path
 
 sys.path.append(str(Path("exts").resolve()))
 
-extensions = ["foo"]
-
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -29,10 +27,8 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "sphinx.ext.autosummary",
-    # "sphinx_toolbox.latex",
 ]
 
-templates_path = ["_templates"]
 exclude_patterns = []
 
 
@@ -40,30 +36,4 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "bizstyle"
-html_static_path = ["_static"]
 html_theme_options = {"sidebarwidth": "410"}
-
-
-## Latex
-latex_engine = "xelatex"
-latex_elements = {
-    "passoptionstopackages": r"""
-\PassOptionsToPackage{svgnames}{xcolor}
-""",
-    "fontpkg": r"""
-\setmainfont{DejaVu Serif}
-\setsansfont{DejaVu Sans}
-\setmonofont{DejaVu Sans Mono}
-""",
-    "preamble": r"""
-\usepackage[titles]{tocloft}
-\cftsetpnumwidth {1.25cm}\cftsetrmarg{1.5cm}
-\setlength{\cftchapnumwidth}{0.75cm}
-\setlength{\cftsecindent}{\cftchapnumwidth}
-\setlength{\cftsecnumwidth}{1.25cm}
-""",
-    "sphinxsetup": "TitleColor=DarkGoldenrod",
-    "fncychap": r"\usepackage[Bjornstrup]{fncychap}",
-    "printindex": r"\footnotesize\raggedright\printindex",
-}
-latex_show_urls = "footnote"
