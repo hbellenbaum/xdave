8eb87365f0378701748e4384a65d31c0d6df47fd
Hannah Bellenbaum <h.bellenbaum@hzdr.de>
2026-04-17 23:42:01 +0200

    changed theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,5 +35,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "bizstyle"
-html_theme_options = {"sidebarwidth": "410"}
+html_theme = "pydata_sphinx_theme"
+# html_theme_options = {"sidebarwidth": "410"}
